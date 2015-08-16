c789a44643d87e57fb18eec0c4bb4a57d3dfb8f7
Arkom Thaicharoen <artchula@MacBook-Pro.local>
2015-08-16 09:11:37 +0700

    Start Day2

---
+++ app/src/main/java/apparduino/artchula/traffic/MainActivity.java
@@ -0,0 +1,96 @@
+package apparduino.artchula.traffic;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initial Widget
+        initialWidget();
+
+        //Button Controller
+        buttonController();
+
+
+    } //Main Method
+
+    private void buttonController() {
+
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_shut);
+                buttonPlayer.start();
+
+                //Intent to webView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://www.genymotion.com"));
+                startActivity(objIntent);
+
+            } //event
+        });
+
+    }
+
+
+    private void initialWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.btxAboutMe);
+
+
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}  //Main Class นี่คือ คลาสหลัก
